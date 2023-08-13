c0650249f0afb3721ea4f4d661a9817a25dfb0d8
Larmbs <138258207+Larmbs@users.noreply.github.com>
2023-08-13 07:55:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ import * as draw from './render.js'
 import {Particle} from './particleHandler.js'
 import {window_height, window_width} from './settings.js';
 
-var g = new Vec(0, -0.5)
+var g = new Vec(0, -0.01)
 
-let pCount = 50
+let pCount = 90
 
 let pList = []
-for (let i = 0; i < pCount; i++) {
+for (let i = 0; i < pCount/3; i++) {
     pList.push(new Particle(Math.random() * (window_width - 100) - (window_width-100)/2, Math.random() * (window_height - 100) - (window_height-100)/2, Math.random() * 20 - 10, Math.random() * 20 - 10, g, 1, 'blue', 'blue', 20))
 }
-for (let i = 0; i < pCount; i++) {
+for (let i = 0; i < pCount/3; i++) {
     pList.push(new Particle(Math.random() * (window_width - 100) - (window_width-100)/2, Math.random() * (window_height - 100) - (window_height-100)/2, Math.random() * 20 - 10, Math.random() * 20 - 10, g, 1, 'red', 'blue', 10))
 }
-for (let i = 0; i < pCount; i++) {
+for (let i = 0; i < pCount/3; i++) {
     pList.push(new Particle(Math.random() * (window_width - 100) - (window_width-100)/2, Math.random() * (window_height - 100) - (window_height-100)/2, Math.random() * 20 - 10, Math.random() * 20 - 10, g, 1, 'green', 'blue', 15))
 }
 
